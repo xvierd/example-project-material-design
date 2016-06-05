b58f8f3b81cf6de218a4825fbe9ad0b693857ff6
Xavier David <xaviergutierrez@maiasoftware.com.ve>
2016-06-04 21:08:23 -0400

    revirtiendo

--- FuffyFef/app/src/main/java/com/example/xavier/fuffyfef/config/Config.java
+++ FuffyFef/app/src/main/java/com/example/xavier/fuffyfef/config/Config.java
@@ -4,6 +4,6 @@ package com.example.xavier.fuffyfef.config;
  * Created by xavier on 02/06/16.
  */
 public class Config {
-    public static final String EMAIL ="****@gmail.com";
+    public static final String EMAIL ="******@gmail.com";
     public static final String PASSWORD ="******";
 }
